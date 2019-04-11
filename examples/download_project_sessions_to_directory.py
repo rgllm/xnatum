7fcd5e5ad4a69ef23326e414c22e5e71a66a3a2a
rgllm <r@rgllm.com>
2019-04-11 23:55:12 +0100

    Added examples to temporary directory

---
+++ examples/download_project_sessions_to_directory.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import argparse
+import sys
+import os
+import glob
+import getpass
+from xnatum import Xnat, util
+import tempfile
+import shutil
+
+def main():
+    parser = argparse.ArgumentParser(description='Download session')
+    parser.add_argument('-project', required=True, help='Project ID')
+    parser.add_argument('-server', help='Server for XNAT connection', required=True)
+    parser.add_argument('-username', help='Username for XNAT connection', required=True)
+    parser.add_argument('-password', help='Password for XNAT connection', required=True)
+    args = parser.parse_args()
+
+    # Creating connection and load experiment object
+    xnat = Xnat(args.server, args.username, args.password)
+    dirpath = tempfile.mkdtemp()
+    xnat_sessions = xnat.download_project_sessions_to_folder(args.project, dirpath)
+    print(xnat_sessions)
+    shutil.rmtree(dirpath)
+
+# init
+if __name__ == '__main__':
+    main()
